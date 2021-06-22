4fcf4028400379ecdfe38b2ca9610eccc4020ca2
Felipe Flores <felipefloresvendas@gmail.com>
2021-06-22 15:30:56 -0300

    feat exercise 1

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,45 +1,46 @@
 // src/context/Provider.js
 
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CarsContext from './CarsContext';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
-  }
-
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
+function Provider(props) {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     cars: {
+  //       red: false,
+  //       blue: false,
+  //       yellow: false,
+  //     },
+  //   }
+  //   this.moveCar = this.moveCar.bind(this);
+  // }
+  
+  let [cars, setCars] = useState({
+    red: false,
+    blue: false,
+    yellow: false,
+  });
+  
+  function moveCar(car, side) {
+    setCars({
+        ...cars,
         [car]: side,
-      },
     });
   };
-
-  render() {
     const context = {
-      ...this.state,
-      moveCar: this.moveCar,
+      ...cars,
+      moveCar,
     };
 
-    const { children } = this.props;
+    const { children } = props;
 
     return (
       <CarsContext.Provider value={context}>
         {children}
       </CarsContext.Provider>
     );
-  }
 };
 
 Provider.propTypes = {
